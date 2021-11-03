587abb552cd2e58c543a0dad87f411fd6a087621
baggio10czm <baggio_tnt@163.com>
2021-11-03 23:50:46 +0800

    修改 README.md

--- FishBook.py
+++ FishBook.py
@@ -5,4 +5,3 @@ app = create_app()
 if __name__ == '__main__':
     # threaded=True 开启多线程
     app.run(host='0.0.0.0', debug=app.config['DEBUG'], port=81, threaded=True)
-
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # FishBook
 学习Python教程，鱼书代码
+
+# pipenv install
+# pipenv shell
+
+# 解决报错
+pip install email-validator
